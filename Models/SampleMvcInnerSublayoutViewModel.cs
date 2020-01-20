d52f91a3e17b90a545c3abe846b5e9f34eda5b4b
Scott Simpson <Scott@simply-simpy.com>
2020-01-20 10:02:49 -0800

    first push

---
+++ Models/SampleMvcInnerSublayoutViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleMvc.Models
+{
+    public class SampleMvcInnerSublayoutViewModel
+    {
+        public int CurrentYear { get; set; }
+    }
+}
