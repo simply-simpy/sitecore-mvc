574428cfb4daeb74938b3c993c21f45592adf27e
Scott Simpson <simply.simpy@gmail.com>
2020-01-24 14:10:58 -0800

    BrowserSync working
    BrowserSync is hotswapping with a local proxy of the Sitecore instance. This requires a virtual directory in IIS. Permissions must be set as shown here: https://dotnet-revanth.blogspot.com/2016/01/iis-permissions-for-virtual-directory.html. Also, auth headers must be set: https://www.centrel-solutions.com/media/xiaconfiguration/adminguideweb/InvalidAuthenticationHeaders.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,31 @@
-﻿function defaultTask(cb) {
-    // place code for your default task here
-    console.log('Gulp is working!')
-    cb();
-}
+﻿//Dependencies
+var gulp = require('gulp');
+var run = require('gulp-run-command').default;
+var bs = require('browser-sync').create(); // create a browser sync instance.
+var sass = require('gulp-sass');
+var scss = "./scss/*.scss";
+var destCss = "./css";
 
-exports.default = defaultTask
+gulp.task('browser-sync', function () {
+    bs.init({
+        proxy: "http://samplemvc.local/" // makes a proxy for localhost:3000
+    });
+});
+
+gulp.task('sass:watch', function () {
+    return gulp.watch(scss, gulp.series('sass'));
+});
+
+gulp.task('sass', function () {
+    console.log('Sass was called!')
+    return gulp.src(scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(destCss))
+        .pipe(bs.stream());
+
+});
+
+gulp.task('default', gulp.parallel('sass', 'sass:watch','browser-sync'), function (done) {
+    browserSync.reload();
+    done();
+});
